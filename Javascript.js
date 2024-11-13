6d98fefce307a3d200ebda61d7aadbcfa11e0523
Mathias Jacobsen <75167111+MathiasRepo@users.noreply.github.com>
2024-11-13 16:16:09 +0100

    Small changes

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+function buttonClick() {
+    let num1 = 3;
+    let num2 = 4;
+
+    let sum = num1 + num2;
+
+    alert(`${num1} + ${num2} = ${sum}`);
+    window.location.reload();
+}
+
+function colorChange() {
+    document.getElementById("box").style.backgroundColor="blue";
+}
